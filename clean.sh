6483035ba3607ff1fc169b7c1293fe2cf804c60e
Konrad Wasowicz <exaroth@gmail.com>
2025-10-01 06:04:12 +0200

    Update clean script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Are you sure? " -n 1 -r
+read -p "This will reset all the changes you made, are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
