f3b774926844cea658b8cd47cbaae590e31e54b3
Konrad Wasowicz <exaroth@gmail.com>
2025-10-02 08:02:15 +0200

    Add path override functionality

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-mkdir -p tmp
-mkdir -p build
-rm -Rf AppDir/wineprefix
-rm -Rf AppDir/game
+rm -Rf AppDir/wineprefix AppDir/game AppDir/overrides
+mkdir -p tmp build AppDir/overrides
 
 cp -Rf ./game ./wineprefix/ AppDir
+
+while read -r p; do
+  src="$(echo "$p" | cut -d ':' -f1)"
+  if [ ! -e "$src" ]; then
+      echo "doesnt exist!"
+      continue
+  fi
+
+  mkdir -p "AppDir/overrides/$(dirname $src)"
+  mv "$src" "AppDir/overrides/$(dirname $src)"
+  ln -s "/tmp/.game_overrides/$src" "$src"
+done < AppDir/override_list
 
 # todo update
 if [ "$1" = "--rw-mode" ]; then
