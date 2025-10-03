d73be0ece86a62b74f5f93aca1e8bd0fe7e6ee7e
Konrad Wasowicz <exaroth@gmail.com>
2025-10-03 02:18:03 +0200

    Use temporary version of wine for provisioning

--- AppDir/provision_wineprefix.sh
+++ AppDir/provision_wineprefix.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This file is used for provisioning wineprefix in --provision-mode
-#
-echo "$WINE"
+
+export WINEDEBUG=${WINEDEBUG:-"fixme-all"}
 
 $WINE wineboot
+
+# Add provisioning for the wineprefix below
+# eg.
+#
+# $WINE winetricks dxvk
