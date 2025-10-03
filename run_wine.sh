05bf296fe1826c732f4d9aeb9cffd7823684a9d2
Konrad Wasowicz <exaroth@gmail.com>
2025-10-03 03:24:28 +0200

    Show error if running wine script without wine

--- run_wine.sh
+++ run_wine.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ ! -f ./AppDir/winedata/wine ]; then
+    echo "Missing wine AppImage, run setup.sh first."
+    exit 1
+fi
+
 WINEPREFIX=$PWD/wineprefix ./AppDir/winedata/wine "$@"
